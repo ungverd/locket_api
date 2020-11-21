e20db4499410d5a0097b63ae9d63235492924edf
Julia Salnikova (Notiel) <agereth@gmail.com>
2020-11-21 19:53:16 +0300

    radio level added to feeling project

--- projects/feeling_behavior.cpp
+++ projects/feeling_behavior.cpp
@@ -2,42 +2,117 @@
 // Created by juice on 21.08.2020.
 //
 #include "feeling_behavior.h"
+#include "utility.h"
 
-const LedRGBChunk kFeelSequence[] = {
-        {ChunkType::kSetup, 0, kMagenta},
-        {ChunkType::kWait, 99},
+const LedRGBChunk kMasterStartSequence[] = {
+        {ChunkType::kSetup, 0, kBlue},
+        {ChunkType::kWait, 299},
         {ChunkType::kSetup, 0, kBlack},
-        {ChunkType::kWait, 99},
-        {ChunkType::kSetup, 0, kMagenta},
-        {ChunkType::kWait, 99},
+        {ChunkType::kWait, 299},
+        {ChunkType::kSetup, 0, kBlue},
+        {ChunkType::kWait, 299},
         {ChunkType::kSetup, 0, kBlack},
-        {ChunkType::kWait, 99},
-        {ChunkType::kSetup, 0, kMagenta},
-        {ChunkType::kWait, 99},
+        {ChunkType::kWait, 299},
+        {ChunkType::kSetup, 0, kBlue},
+        {ChunkType::kWait, 299},
         {ChunkType::kSetup, 0, kBlack},
         {ChunkType::kEnd}
 };
 
-
+const LedRGBChunk kSlaveStartSequence[] = {
+        {ChunkType::kSetup, 0, kRed},
+        {ChunkType::kWait, 299},
+        {ChunkType::kSetup, 0, kBlack},
+        {ChunkType::kWait, 299},
+        {ChunkType::kSetup, 0, kRed},
+        {ChunkType::kWait, 299},
+        {ChunkType::kSetup, 0, kBlack},
+        {ChunkType::kWait, 299},
+        {ChunkType::kSetup, 0, kRed},
+        {ChunkType::kWait, 299},
+        {ChunkType::kSetup, 0, kBlack},
+        {ChunkType::kEnd}
+};
 
 void FeelingBehavior::OnStarted() {
     logger->log("Started execution!");
-    led->StartOrRestart(kStartSequence);
-    radio->SetBeaconPacket({19});
 }
 
 void FeelingBehavior::EverySecond() {
-    ++seconds_counter;
-    if (rx_table.HasPacketWithId(19) && (seconds_counter % 5 == 0)) {
-        vibro->StartOrRestart(kBrrBrrBrr);
-        led->StartOrRestart(kFeelSequence);
-        rx_table.Clear();
-        logger->log("BrrBrrBrr");
+    if (LocketType == SLAVE) {
+        ++seconds_counter;
+        if (rx_table.HasPacketWithId(1) && (seconds_counter % 10 == 0)) {
+            vibro->StartOrRestart(kBrrBrrBrr);
+            rx_table.Clear();
+            logger->log("BrrBrrBrr");
+            seconds_counter = 0;
+        }
     }
-    logger->log("PacketSent!");
-
 }
 
 void FeelingBehavior::OnRadioPacketReceived(const IdOnlyState& packet) {
     rx_table.AddPacket(packet);
+}
+
+void FeelingBehavior::OnDipSwitchChanged(uint16_t dip_value_mask) {
+    if (GetSwitchState(dip_value_mask, 1)) {
+        led->StartOrRestart(kMasterStartSequence);
+        radio->SetBeaconPacket({1});
+        LocketType = MASTER;
+    } else {
+        led->StartOrRestart(kSlaveStartSequence);
+        radio->ClearBeaconPacket();
+        LocketType = SLAVE;
+    }
+    uint8_t first_range = GetSwitchState(dip_value_mask, 5);
+    uint8_t second_range = GetSwitchState(dip_value_mask, 6);
+    uint8_t third_range = GetSwitchState(dip_value_mask, 7);
+    uint8_t fourth_range = GetSwitchState(dip_value_mask, 8);
+    uint8_t range = first_range + 2*second_range + 4*third_range * 8*fourth_range;
+    if (range > 11) {
+        range = 11;
+    }
+    RangeLevel = TypeToEnum(range);
+    radio->SetPowerLevel(RangeLevel);
+    logger->log("DIP switch changed to %d%d%d%d%d%d%d%d",
+               GetSwitchState(dip_value_mask, 1),
+               GetSwitchState(dip_value_mask, 2),
+               GetSwitchState(dip_value_mask, 3),
+               GetSwitchState(dip_value_mask, 4),
+               GetSwitchState(dip_value_mask, 5),
+               GetSwitchState(dip_value_mask, 6),
+               GetSwitchState(dip_value_mask, 7),
+               GetSwitchState(dip_value_mask, 8));
+}
+
+
+RadioPowerLevel TypeToEnum(uint8_t id) {
+    switch (id) {
+        case 0:
+            return RadioPowerLevel::MINUS_30_DBM;
+        case 1:
+            return RadioPowerLevel::MINUS_27_DBM;
+        case 2:
+            return RadioPowerLevel::MINUS_25_DBM;
+        case 3:
+            return RadioPowerLevel::MINUS_20_DBM;
+        case 4:
+            return RadioPowerLevel::MINUS_15_DBM;
+        case 5:
+            return RadioPowerLevel::MINUS_10_DBM;
+        case 6:
+            return RadioPowerLevel::MINUS_6_DBM;
+        case 7:
+            return RadioPowerLevel::PLUS_0_DBM;
+        case 8:
+            return RadioPowerLevel::PLUS_5_DBM;
+        case 9:
+            return RadioPowerLevel::PLUS_7_DBM;
+        case 10:
+            return RadioPowerLevel::PLUS_10_DBM;
+        case 11:
+            return RadioPowerLevel::PLUS_12_DBM;
+        default:
+            return RadioPowerLevel::PLUS_0_DBM;
+    }
 }
--- projects/feeling_behavior.h
+++ projects/feeling_behavior.h
@@ -9,6 +9,10 @@
 #include "rx_table.h"
 #include "behavior.h"
 
+
+#define MASTER 1
+#define SLAVE 0
+
 class FeelingBehavior: public Behavior<IdOnlyState, IdOnlyState> {
 public:
     using Behavior::Behavior;
@@ -16,12 +20,15 @@ public:
     void OnStarted() override;
     void EverySecond() override;
     void OnRadioPacketReceived(const IdOnlyState& packet) override;
+    void OnDipSwitchChanged(uint16_t dip_value_mask) override;
 
 private:
     uint32_t seconds_counter = 0;
     RxTable<IdOnlyState> rx_table;
-
+    uint8_t LocketType = SLAVE;
+    RadioPowerLevel RangeLevel = RadioPowerLevel::MINUS_6_DBM;
 };
 
+RadioPowerLevel TypeToEnum(uint8_t level);
 
 #endif //LOCKET_API_FEELING_BEHAVIOR_H
