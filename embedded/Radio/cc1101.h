a9cb21087d5a20ffc2848464b0974d3cc436df44
Alexey Eremin <a.eremin.msu@gmail.com>
2021-06-26 23:38:07 +0200

    Import CC1101 driver from https://github.com/Kreyl/Locket_fw/tree/FeelEachOtherGoldenDisc

--- embedded/Radio/cc1101.h
+++ embedded/Radio/cc1101.h
@@ -24,6 +24,8 @@ private:
     const PinIrq_t IGdo0;
     uint8_t IState; // Inner CC state, returned as first byte
     thread_reference_t ThdRef;
+    ftVoidPVoid ICallback = nullptr;
+    void* user_data_ = nullptr;
     // Pins
     uint8_t BusyWait() {
         for(uint32_t i=0; i<CC_BUSYWAIT_TIMEOUT; i++) {
@@ -34,7 +36,6 @@ private:
     void CsHi() { PinSetHi((GPIO_TypeDef*)CSGpio, Cs); }
     void CsLo() { PinSetLo((GPIO_TypeDef*)CSGpio, Cs); }
     // General
-    void RfConfig();
     int8_t RSSI_dBm(uint8_t ARawRSSI);
     // Registers and buffers
     uint8_t WriteRegister(const uint8_t Addr, const uint8_t AData);
@@ -46,6 +47,8 @@ private:
     uint8_t EnterTX()     { return WriteStrobe(CC_STX);  }
     uint8_t EnterRX()     { return WriteStrobe(CC_SRX);  }
     uint8_t FlushRxFIFO() { return WriteStrobe(CC_SFRX); }
+    uint8_t FlushTxFIFO() { return WriteStrobe(CC_SFTX); }
+    uint8_t GetStatus()   { return WriteStrobe(CC_SNOP); }
 public:
     uint8_t Init();
     uint8_t EnterIdle()    { return WriteStrobe(CC_SIDLE); }
@@ -53,30 +56,46 @@ public:
     void SetChannel(uint8_t AChannel);
     void SetTxPower(uint8_t APwr)  { WriteRegister(CC_PATABLE, APwr); }
     void SetPktSize(uint8_t ASize) { WriteRegister(CC_PKTLEN, ASize); }
+    void SetBitrate(const CCRegValue_t* BRSetup);
     // State change
     void Transmit(void *Ptr, uint8_t Len);
     uint8_t Receive(uint32_t Timeout_ms, void *Ptr, uint8_t Len,  int8_t *PRssi=nullptr);
+    uint8_t Receive_st(sysinterval_t Timeout_st, void *Ptr, uint8_t Len,  int8_t *PRssi=nullptr);
+    void ReceiveAsync(ftVoidPVoid Callback, void* user_data);
+
+    uint8_t RxCcaTx_st(void *PtrTx, uint8_t Len,  int8_t *PRssi=nullptr);
+    uint8_t RxIfNotYet_st(sysinterval_t RxTimeout_st, void *Ptr, uint8_t Len,  int8_t *PRssi=nullptr);
+
     void PowerOff();
     uint8_t Recalibrate() {
         while(IState != CC_STB_IDLE) {
             if(EnterIdle() != retvOk) return retvFail;
         }
         if(WriteStrobe(CC_SCAL) != retvOk) return retvFail;
-        return BusyWait();
+        do {
+            GetStatus();
+        } while(IState != CC_STB_IDLE);
+        return retvOk;
     }
+    // Setup
+//    void DoRxAfterRxAndRxAfterTx()   { WriteRegister(CC_MCSM1, (CC_MCSM1_VALUE | 0x0F)); }
+//    void DoRxAfterRxAndIdleAfterTx() { WriteRegister(CC_MCSM1, ((CC_MCSM1_VALUE | 0x0C) & 0xFC)); }
+    void DoRxAfterTx()   { WriteRegister(CC_MCSM1, (CC_MCSM1_VALUE | 0x03)); }
+    void DoIdleAfterTx() { WriteRegister(CC_MCSM1, CC_MCSM1_VALUE); }
+
     uint8_t ReadFIFO(void *Ptr, int8_t *PRssi, uint8_t Len);
 
-    void IIrqHandler() { chThdResumeI(&ThdRef, MSG_OK); }   // NotNull check perfprmed inside chThdResumeI
+    void IIrqHandler();
 
     cc1101_t(
             SPI_TypeDef *ASpi, GPIO_TypeDef *ASpiGpio,
             uint16_t ASck, uint16_t AMiso, uint16_t AMosi,
             GPIO_TypeDef *ACSGpio, uint16_t ACs,
             GPIO_TypeDef *AGd0Gpio, uint16_t AGdo0):
-        ISpi(ASpi), SpiGpio(ASpiGpio), CSGpio(ACSGpio),
-        Sck(ASck), Miso(AMiso), Mosi(AMosi), Cs(ACs),
-        IGdo0(AGd0Gpio, AGdo0, pudNone, CCIrqHandler),
-        IState(0), ThdRef(nullptr) {}
+            ISpi(ASpi), SpiGpio(ASpiGpio), CSGpio(ACSGpio),
+            Sck(ASck), Miso(AMiso), Mosi(AMosi), Cs(ACs),
+            IGdo0(AGd0Gpio, AGdo0, pudNone, CCIrqHandler),
+            IState(0), ThdRef(nullptr) {}
 };
 
 extern cc1101_t CC;
