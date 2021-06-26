2ccf2ed6e1b0db0d23c529240fc3e1687a7fd67b
Alexey Eremin <a.eremin.msu@gmail.com>
2021-06-26 23:44:08 +0200

    Reserve some storage in eeprom for low-level settings (e.g. id)

--- embedded/wrappers/eeprom_wrapper.cpp
+++ embedded/wrappers/eeprom_wrapper.cpp
@@ -2,10 +2,15 @@
 
 #include "embedded/kl_lib/kl_lib.h"
 
+namespace {
+    // First 512 bytes are reserved for low-level data.
+    uint32_t kHighLevelDataOffset = 512;
+}
+
 uint32_t EepromWrapper::ReadUint32(uint32_t address) {
-    return EE::Read32(address);
+    return EE::Read32(kHighLevelDataOffset + address);
 }
 
 uint8_t EepromWrapper::WriteUint32(uint32_t address, uint32_t value) {
-    return EE::Write32(address, value);
+    return EE::Write32(kHighLevelDataOffset + address, value);
 }
